a9472ad0ca414ed797e7f92c48a42950b056ccfa
Madhu-R-cloud <madhusudanr00@gmail.com>
2023-12-14 16:53:56 +0530

    Fundoo-Note : Registraion API

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,47 @@
+import HttpStatus from 'http-status-codes';
+import * as UserService from '../services/user.service';
+import date from '@hapi/joi/lib/types/date';
+
+/**
+ * Controller to get all users available
+ * @param  {object} req - request object
+ * @param {object} res - response object
+ * @param {Function} next
+ */
+export const getAllUsers = async (req, res, next) => {
+  try {
+    const data = await UserService.getAllUsers();
+    res.status(HttpStatus.OK).json({
+      code: HttpStatus.OK,
+      data: data,
+      message: 'All users fetched successfully'
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+
+/**
+ * Controller to create a new user
+ * @param  {object} req - request object
+ * @param {object} res - response object
+ * @param {Function} next
+ */
+export const newUser = async (req, res, next) => {
+  
+    const data = await UserService.newUser(req.body);
+    if(!Error){
+      res.status(HttpStatus.CREATED).json({
+        code: HttpStatus.CREATED,
+        data: data,
+        message: 'User Registraion is successfully'
+      });
+    }else{
+      res.status(409).json({
+        code : 409,
+        message: 'User with the same email already exists, Please Login'
+      });
+    }
+};
---
+++ src/routes/user.route.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import * as userController from '../controllers/user.controller';
+import { newUserValidator } from '../validators/user.validator';
+import { userAuth } from '../middlewares/auth.middleware';
+
+const router = express.Router();
+
+//route to get all users
+router.get('', userController.getAllUsers);
+
+//route to create a new user
+router.post('/register', newUserValidator, userController.newUser);
+
+
+export default router;
